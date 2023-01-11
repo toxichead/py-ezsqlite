d26a4d83eb8dde798b8fc3be39b44acfad501e17
toxichead <55358751+toxichead@users.noreply.github.com>
2023-01-11 03:38:42 +0300

    Add files via upload

---
+++ ezsqlite.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+class EazySQLite3():
+    def __init__(self, db: str = "sqlite.db"):
+        self.connection = sqlite3.connect(db)
+        #self.cursor = sqlite_connection.cursor()
+    def query(self, query: str = ""):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(query)
+            self.connection.commit()
+            try:
+                record = cursor.fetchall()
+                cursor.close()
+                return {"status": True, "details": record} 
+            except:
+                try: cursor.close()
+                except: pass
+                finally: return {"status": True, "details": None} 
+        except Exception as e: return {"status": False, "details": str(e)} 
+    def close(self):
+        self.connection.close()
